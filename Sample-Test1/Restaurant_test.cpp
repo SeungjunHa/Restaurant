3a0d6bf2bbc2ade5280da9d69b201e2129b8315b
SeungjunHa <gkak1121@naver.com>
2024-05-20 13:50:20 +0900

    Rename Test File & Add TC #1

---
+++ Sample-Test1/Restaurant_test.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Project5/Customer.cpp"
+#include "../Project5/Schedule.cpp"
+#include "../Project5/BookingScheduler.cpp"
+
+TEST(BookingSchedulerTest, OnlyOnTheHour)
+{
+	tm notOnTheHour = { 0 };
+	notOnTheHour.tm_year = 2021 - 1900;
+	notOnTheHour.tm_mon = 03 - 1;
+	notOnTheHour.tm_mday = 26;
+	notOnTheHour.tm_hour = 9;
+	notOnTheHour.tm_min = 5;
+	notOnTheHour.tm_isdst = -1;
+	mktime(&notOnTheHour);
+
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	EXPECT_THROW({
+		bookingScheduler.addSchedule(schedule);
+		}, std::runtime_error);
+}
