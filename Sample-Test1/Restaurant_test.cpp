511643e22d1eb83578a53648e18ddd5d318d0923
SeungjunHa <gkak1121@naver.com>
2024-05-20 13:57:37 +0900

    Code Refactoring - Move to Fixture for frequently used variables

--- Sample-Test1/Restaurant_test.cpp
+++ Sample-Test1/Restaurant_test.cpp
@@ -7,18 +7,25 @@
 using namespace testing;
 
 class BookingItem : public Test {
+protected:
+	void SetUp() override {
+		notOnTheHour = getTime(2021, 3, 26, 9, 5);
+		onTheHour = getTime(2021, 3, 26, 9, 0);
+	}
 public:
 	tm getTime(int year, int mon, int day, int hour, int min) {
 		tm result = { 0, min, hour, day, mon - 1, year - 1900, 0, 0, -1 };
 		mktime(&result);
 		return result;
 	}
+
+	tm notOnTheHour;
+	tm onTheHour;
+	Customer customer{ "Fake Name", "010-1234-5678" };
 };
 
 TEST_F(BookingItem, OnlyOnTheHourFail)
 {
-	tm notOnTheHour = getTime(2021, 3, 26, 9, 5);
-	Customer customer{ "Fake name", "010-1234-5678" };
 	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
 	BookingScheduler bookingScheduler{ 3 };
 
@@ -29,8 +36,6 @@ TEST_F(BookingItem, OnlyOnTheHourFail)
 
 TEST_F(BookingItem, OnlyOnTheHourPass)
 {
-	tm onTheHour = getTime(2021, 3, 26, 9, 0);
-	Customer customer{ "Fake name", "010-1234-5678" };
 	Schedule* schedule = new Schedule{ onTheHour, 1, customer };
 	BookingScheduler bookingScheduler{ 3 };
 
