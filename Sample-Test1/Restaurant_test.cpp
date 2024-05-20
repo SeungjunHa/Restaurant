1312afa183ed9afff9fe9fc42583e441a50c53f4
SeungjunHa <gkak1121@naver.com>
2024-05-20 13:52:13 +0900

    Add TC #2 - Can make a reservation on the hour.

--- Sample-Test1/Restaurant_test.cpp
+++ Sample-Test1/Restaurant_test.cpp
@@ -4,7 +4,7 @@
 #include "../Project5/Schedule.cpp"
 #include "../Project5/BookingScheduler.cpp"
 
-TEST(BookingSchedulerTest, OnlyOnTheHour)
+TEST(BookingSchedulerTest, OnlyOnTheHourFail)
 {
 	tm notOnTheHour = { 0 };
 	notOnTheHour.tm_year = 2021 - 1900;
@@ -22,4 +22,25 @@ TEST(BookingSchedulerTest, OnlyOnTheHour)
 	EXPECT_THROW({
 		bookingScheduler.addSchedule(schedule);
 		}, std::runtime_error);
+}
+
+
+TEST(BookingSchedulerTest, OnlyOnTheHourPass)
+{
+	tm onTheHour = { 0 };
+	onTheHour.tm_year = 2021 - 1900;
+	onTheHour.tm_mon = 03 - 1;
+	onTheHour.tm_mday = 26;
+	onTheHour.tm_hour = 9;
+	onTheHour.tm_min = 0;
+	onTheHour.tm_isdst = -1;
+	mktime(&onTheHour);
+
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ onTheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	bookingScheduler.addSchedule(schedule);
+
+	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
